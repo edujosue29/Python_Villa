6d39c542914e66a2c5657578885e581e1d3a1864
Allan Rojas <allanjose91@gmail.com>
2015-11-20 22:06:02 -0600

    Changes Allan
    Validation Commit

---
+++ Validaciones/Proyecto2.py
@@ -0,0 +1,75 @@
+import csv
+import Validaciones as va
+
+
+
+#Lista para guardar los valores que lee del .CSV
+List = []  
+
+#Numero de campos requeridos de la lista
+Campos = 7 
+
+  
+#Validacion de Campo, Recibe una Lista Que Representa a Los Campos de Un Avion
+def check_campo(List):
+    print ("\n___________________________________\n") 
+    print ('-- Checkeando Avion :' + List[0] +'--')  
+    count = 1 
+    checkG = True
+
+    if len(List)== Campos :
+        for campo in List: 
+            check = va.validate(campo,count)
+            if check==False:
+                checkG = False
+            count = count + 1   
+                      
+    else :
+          print('Campos insuficientes')
+          checkG = False;          
+    
+    return (checkG)
+     
+    
+
+def read():
+    try:
+        with open('estacion.csv', newline='') as f:
+         reader = csv.reader(f)
+         for row in reader:
+          List.append(row)    
+    except:
+         print ('El archivo no existe')
+   
+
+
+def main():
+    read()
+    if not List:
+      print ("No hay datos en el archivo")
+    else :
+        count = 0
+        length = len(List)
+        while (count != length):
+         avion = List[count]
+         
+         #Si pasa
+         if(check_campo(avion)):
+         
+            print ('--El Avion: ' + avion[0] + ' Fue Permitido\n Coordenadas Entrada (' + avion[1] + ','+ avion[2] +')' +
+                                     '\n Coordenadas Salida (' + avion[3] + ','+ avion[4] +')' +
+                                      '\n MinutoEntrada (' + avion[5] + ')'+
+                                       '\n Velocidad (' + avion[6] + ')'     
+                )  
+         else:
+          
+            print ('--El Avion: ' + avion[0] + ' No puede ingresar a la zona') 
+        
+        
+         count = count + 1
+
+        #print ("Encontrados: " ,  length)  
+    
+
+#llamada a funcion principal
+main()    
---
+++ Validaciones/Validaciones.py
@@ -0,0 +1,70 @@
+#Variables de rango
+rango_min = -125
+rango_max = 125
+punto_entrada = 25 
+
+#Validaciones para punto de entrada
+def validation(campo):
+    if( (int(campo)%punto_entrada==0) and  
+        (int(campo)>=rango_min) and 
+       ( int(campo)<=rango_max )):
+        return True
+    else :
+      return False
+  
+#Validaciones para el tiempo 
+def validation_time(campo):
+
+        return True
+ 
+def validation_speed(campo):
+    
+       return True
+
+# Validaciones  a cada campo
+
+    #Chequear Xin ######################################################
+def validate(campo,count):
+    check = True
+    if count == 2:
+        if ( validation(campo)) :
+         print('Campo Xin Perm',campo)
+        else :
+            print('Campo Xin N-Perm',campo)
+            check = False
+     #Chequear Yin #####################################################       
+    if count == 3:
+        if ( validation(campo)) :
+         print('Campo Yin Perm',campo)
+        else :
+            print('Campo Yin N-Perm',campo)
+            check = False  
+     #Chequear Xout ####################################################        
+    if count == 4:
+         if ( validation(campo)) :
+            print('Campo Xout Perm',campo)
+         else :
+            print('Campo Xout N-Perm',campo)
+            check = False
+     #Chequear Yout ####################################################       
+    if count == 5:
+        if ( validation(campo)) :
+            print('Campo Yout Perm',campo)
+        else :
+            print('Campo Yout N-Perm',campo)
+            check = False
+     #Chequear Time ####################################################        
+    if count == 6:
+        if ( validation_time(campo)) :
+            print('Campo Time Perm',campo)
+        else :
+            print('Campo Time N-Perm',campo)
+            check = False
+    if count == 7:
+        if ( validation_speed(campo)) :
+            print('Campo Speed Perm',campo)
+        else :
+            print('Campo Speed N-Perm',campo)
+            check = False
+
+    return check
